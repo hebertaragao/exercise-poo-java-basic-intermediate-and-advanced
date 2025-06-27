6da4ae88368500606007ba64ce5234aecf5bceed
hebertaragao <hebert.aragao@outlook.com>
2025-06-27 16:18:26 -0300

    Update Carro.java

--- java-exercicios-poo/src/bloco01_basico/Carro.java
+++ java-exercicios-poo/src/bloco01_basico/Carro.java
@@ -2,4 +2,20 @@ package bloco01_basico;
 
 public class Carro {
 
+    String marca;
+    String modelo;
+    int ano;
+
+    void exibirDados(){
+        System.out.println(marca + " " + modelo + " " + ano);
+    }
+
+    public static void main(String[] args){
+        Carro c = new Carro();
+        c.marca = "Toyota";
+        c.modelo = "Corolla";
+        c.ano = 2021;
+        c.exibirDados();
+    }
+
 }
