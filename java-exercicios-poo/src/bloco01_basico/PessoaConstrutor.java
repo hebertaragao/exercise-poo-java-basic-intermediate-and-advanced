725f693efb49c86ed2bb2c3af0b5f13a7ab317c7
hebertaragao <hebert.aragao@outlook.com>
2025-06-27 15:59:38 -0300

    Update PessoaConstrutor.java

--- java-exercicios-poo/src/bloco01_basico/PessoaConstrutor.java
+++ java-exercicios-poo/src/bloco01_basico/PessoaConstrutor.java
@@ -1,5 +1,21 @@
 package bloco01_basico;
 
 public class PessoaConstrutor {
+    String nome;
+    int idade;
+
+    public PessoaConstrutor(String nome, int idade){
+        this.nome = nome;
+        this.idade = idade;
+    }
+
+    void exibirDados(){
+        System.out.println(nome + " tem " + idade + " anos.");
+    }
+
+    public static void main(String[] args){
+        PessoaConstrutor p = new PessoaConstrutor("Hebert Aragão", 49);
+        p.exibirDados();
+    }
 
 }
