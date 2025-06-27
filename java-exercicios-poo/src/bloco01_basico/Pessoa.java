7ae0678a50e5dbdbb9445feee5a27dae7837e518
hebertaragao <hebert.aragao@outlook.com>
2025-06-27 15:54:01 -0300

    Update Pessoa.java

--- java-exercicios-poo/src/bloco01_basico/Pessoa.java
+++ java-exercicios-poo/src/bloco01_basico/Pessoa.java
@@ -1,4 +1,19 @@
 package bloco01_basico;
 public class Pessoa {
 
+    String nome;
+    int idade;
+
+    void exibirDados(){
+        System.out.println("Nome: "  + nome);
+        System.out.println("Idade: " + idade);
+    }
+
+    public static void main(String[] args){
+        Pessoa p = new Pessoa();
+        p.nome = "Hebert Aragão";
+        p.idade = 49;
+        p.exibirDados();
+    }
+
 }
