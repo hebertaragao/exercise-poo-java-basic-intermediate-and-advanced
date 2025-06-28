774fc1ed29d9690dba561e549f828b7d12e92599
hebertaragao <hebert.aragao@outlook.com>
2025-06-28 13:56:35 -0300

    Update ProdutoValidado.java class

--- java-exercicios-poo/src/bloco01_basico/ProdutoValidado.java
+++ java-exercicios-poo/src/bloco01_basico/ProdutoValidado.java
@@ -2,4 +2,21 @@ package bloco01_basico;
 
 public class ProdutoValidado {
 
+    String nome;
+    double preco;
+
+    public ProdutoValidado(String nome, double preco){
+        this.nome = nome;
+        this.preco = preco > 0? preco : 1.0;
+    }
+
+    public void exibir(){
+        System.out.println("Produto: " + nome + " Preço: R$ " + preco);
+    }
+
+    public static void main(String[] args){
+        ProdutoValidado p = new ProdutoValidado("Teclado", -20);
+        p.exibir();
+    }
+
 }
