87bb2f9c058ed3f05066a4cccabe99f3fa56b4ef
hebertaragao <hebert.aragao@outlook.com>
2025-06-28 13:47:44 -0300

    Update ProdutoTotal.java class

--- java-exercicios-poo/src/bloco01_basico/ProdutoTotal.java
+++ java-exercicios-poo/src/bloco01_basico/ProdutoTotal.java
@@ -1,5 +1,20 @@
 package bloco01_basico;
 
 public class ProdutoTotal {
+    String nome;
+    double preco;
+    int quantidade;
+
+    double calcularTotal(){
+        return preco * quantidade;
+    }
+
+    public static void main(String[] args){
+        ProdutoTotal p = new ProdutoTotal();
+        p.nome = "Mouse";
+        p.preco = 50.0;
+        p.quantidade = 3;
+        System.out.println("Total> R$ " + p.calcularTotal());
+    }
 
 }
