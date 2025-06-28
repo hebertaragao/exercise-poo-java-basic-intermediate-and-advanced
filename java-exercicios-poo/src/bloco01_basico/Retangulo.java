15fb4322e39bcf06da37d2833211166bf7c5f60d
hebertaragao <hebert.aragao@outlook.com>
2025-06-28 14:07:24 -0300

    Update Retangulo.java class

--- java-exercicios-poo/src/bloco01_basico/Retangulo.java
+++ java-exercicios-poo/src/bloco01_basico/Retangulo.java
@@ -2,4 +2,23 @@ package bloco01_basico;
 
 public class Retangulo {
 
+    double largura;
+    double altura;
+
+    double calcularArea(){
+        return largura * altura;
+    }
+
+    double calcularPerimetro(){
+        return 2 * (largura + altura);
+    }
+
+    public static void main(String[] args){
+        Retangulo r = new Retangulo();
+        r.largura = 5;
+        r.altura = 3;
+        System.out.println("Área: " + r.calcularArea());
+        System.out.println("Perímetro: " + r.calcularPerimetro());
+    }
+
 }
