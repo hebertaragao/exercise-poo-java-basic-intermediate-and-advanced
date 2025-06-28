7582be6f43820a3a61535920fe75e65586cc5e2c
hebertaragao <hebert.aragao@outlook.com>
2025-06-28 14:16:22 -0300

    Update Livro.java class

--- java-exercicios-poo/src/bloco01_basico/Livro.java
+++ java-exercicios-poo/src/bloco01_basico/Livro.java
@@ -2,4 +2,21 @@ package bloco01_basico;
 
 public class Livro {
 
+    String titulo;
+    String autor;
+    int ano;
+
+    void exibirInfo(){
+        System.out.println(titulo + " - " + autor + " (" + ano + ")");
+
+    }
+
+    public static void main(String[] args){
+        Livro l = new Livro();
+        l.titulo = "Dom Casmurro";
+        l.autor = "Machado de Assis";
+        l.ano = 1899;
+        l.exibirInfo();
+    }
+
 }
