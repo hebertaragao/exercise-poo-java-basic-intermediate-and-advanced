235c62fea5392f38c469e23bc16873dcb145a261
hebertaragao <hebert.aragao@outlook.com>
2025-06-28 14:11:29 -0300

    Update ContaSimples.java class

--- java-exercicios-poo/src/bloco01_basico/ContaSimples.java
+++ java-exercicios-poo/src/bloco01_basico/ContaSimples.java
@@ -2,4 +2,22 @@ package bloco01_basico;
 
 public class ContaSimples {
 
+    double saldo;
+
+    void depositar(double valor){
+        saldo += valor;
+    }
+
+    void sacar(double valor){
+        if (valor <= saldo) saldo -= valor;
+        else System.out.println("Saldo insuficiente.");
+    }
+
+    public static void main(String[] args){
+        ContaSimples c = new ContaSimples();
+        c.depositar(100);
+        c.sacar(30);
+        System.out.println("Salfo final: R$ " + c.saldo);
+    }
+
 }
