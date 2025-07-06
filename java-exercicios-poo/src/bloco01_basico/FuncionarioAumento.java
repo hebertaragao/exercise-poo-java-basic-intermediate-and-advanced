0f896a519028a38aa608c50925ae86c061364a98
hebertaragao <hebert.aragao@outlook.com>
2025-07-06 14:35:11 -0300

    Update TFuncionarioAumento.java class

--- java-exercicios-poo/src/bloco01_basico/FuncionarioAumento.java
+++ java-exercicios-poo/src/bloco01_basico/FuncionarioAumento.java
@@ -2,4 +2,23 @@ package bloco01_basico;
 
 public class FuncionarioAumento {
 
+    String nome;
+    double salario;
+
+    public void aplicarAumento(double percentual){
+        salario += salario * percentual / 100;
+    }
+
+    public void exibirSalario(){
+        System.out.println("Salário atualizado: R$ " + salario);
+    }
+
+    public static void main(String[] args){
+        FuncionarioAumento f = new FuncionarioAumento();
+        f.nome = "Hebert Aragão";
+        f.salario = 2000;
+        f.aplicarAumento(10);
+        f.exibirSalario();
+    }
+
 }
