62c1d7aee7e8e74cbc2ffbbfec6a5282c8015030
hebertaragao <hebert.aragao@outlook.com>
2025-07-06 13:28:18 -0300

    Update ConstrutoresMultiplos.java class

--- java-exercicios-poo/src/bloco01_basico/ConstrutoresMultiplos.java
+++ java-exercicios-poo/src/bloco01_basico/ConstrutoresMultiplos.java
@@ -1,5 +1,28 @@
 package bloco01_basico;
 
 public class ConstrutoresMultiplos {
+    String nome;
+    int idade;
+
+    public ConstrutoresMultiplos(){
+        nome = "Desconhecido";
+        idade = 0;
+}
+
+    public ConstrutoresMultiplos(String nome, int idade){
+        this.nome = nome;
+        this.idade = idade;
+    }
+
+    public void exibir(){
+        System.out.println(nome + ", " + idade + " anos");
+    }
+
+    public static void main(String[] args){
+        ConstrutoresMultiplos p1 = new ConstrutoresMultiplos();
+        ConstrutoresMultiplos p2 = new ConstrutoresMultiplos("Hebert", 25);
+        p1.exibir();
+        p2.exibir();
+    }
 
 }
