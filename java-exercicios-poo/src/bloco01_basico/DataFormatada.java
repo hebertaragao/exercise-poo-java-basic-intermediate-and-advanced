89b75420553f8695d4cefe02fba5ff1836c6a920
hebertaragao <hebert.aragao@outlook.com>
2025-07-06 13:46:36 -0300

    Update DataFormatada.java class

--- java-exercicios-poo/src/bloco01_basico/DataFormatada.java
+++ java-exercicios-poo/src/bloco01_basico/DataFormatada.java
@@ -2,4 +2,18 @@ package bloco01_basico;
 
 public class DataFormatada {
 
+    int dia, mes, ano;
+
+    void exibirData(){
+        System.out.printf("%02d/%02d/%04d\n", dia, mes, ano);
+    }
+
+    public static void main(String[] args){
+        DataFormatada d = new DataFormatada();
+        d.dia = 27;
+        d.mes = 6;
+        d.ano = 2025;
+        d.exibirData();
+    }
+
 }
