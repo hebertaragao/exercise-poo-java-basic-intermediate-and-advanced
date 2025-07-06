179858ec497ba2dce99d5287dca829daaf1dbc5e
hebertaragao <hebert.aragao@outlook.com>
2025-07-06 14:44:51 -0300

    Update Circulo.java class

--- java-exercicios-poo/src/bloco01_basico/Circulo.java
+++ java-exercicios-poo/src/bloco01_basico/Circulo.java
@@ -2,4 +2,21 @@ package bloco01_basico;
 
 public class Circulo {
 
+    double raio;
+
+    public double calcularArea(){
+        return Math.PI * raio * raio;
+    }
+
+    public double calcularCircunferencia(){
+        return 2 * Math.PI * raio;
+    }
+
+    public static void main(String[] args){
+        Circulo c = new Circulo();
+        c.raio = 5;
+        System.out.println("Área: " + c.calcularArea());
+        System.out.println("Circunsferência: " + c.calcularCircunferencia());
+    }
+
 }
