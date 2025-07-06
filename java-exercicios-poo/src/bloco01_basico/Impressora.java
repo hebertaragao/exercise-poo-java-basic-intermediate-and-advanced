14273ca3a946386f8a7ca068289bfdb97797e6a5
hebertaragao <hebert.aragao@outlook.com>
2025-07-06 14:40:16 -0300

    Update Impressora.java class

--- java-exercicios-poo/src/bloco01_basico/Impressora.java
+++ java-exercicios-poo/src/bloco01_basico/Impressora.java
@@ -2,4 +2,21 @@ package bloco01_basico;
 
 public class Impressora {
 
+    int folhasDisponiveis =5;
+
+    public void imprimir(String documento){
+        if(folhasDisponiveis > 0){
+            System.out.println("Imprimindo: " + documento);
+            folhasDisponiveis--;
+        } else{
+            System.out.println("Sem papel!");
+        }
+    }
+
+    public static void main(String[] args){
+        Impressora imp = new Impressora();
+        imp.imprimir("Contrato");
+        imp.imprimir("Relatório");
+    }
+
 }
