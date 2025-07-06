682d3ce403e7270d8c85cdff2a7c69370676561e
hebertaragao <hebert.aragao@outlook.com>
2025-07-06 13:20:19 -0300

    Update Sobrecarga.java class

--- java-exercicios-poo/src/bloco01_basico/Sobrecarga.java
+++ java-exercicios-poo/src/bloco01_basico/Sobrecarga.java
@@ -1,5 +1,18 @@
 package bloco01_basico;
 
 public class Sobrecarga {
+    int somar(int a, int b) {
+        return a + b;
+    }   
+
+    double somar(double a, double b){
+        return a + b;
+    }
+
+    public static void main(String[] args){
+        Sobrecarga s = new Sobrecarga();
+        System.out.println(s.somar(5,3));
+        System.out.println(s.somar(2.5, 4.7));
+    }
 
 }
