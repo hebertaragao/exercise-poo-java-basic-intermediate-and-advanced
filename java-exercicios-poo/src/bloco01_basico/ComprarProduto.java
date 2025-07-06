f21ab776e0418bcded7d8f0e5b8b905de469424e
hebertaragao <hebert.aragao@outlook.com>
2025-07-06 13:36:26 -0300

    Update ComprarProduto.java class

--- java-exercicios-poo/src/bloco01_basico/ComprarProduto.java
+++ java-exercicios-poo/src/bloco01_basico/ComprarProduto.java
@@ -2,4 +2,22 @@ package bloco01_basico;
 
 public class ComprarProduto {
 
+    String nome;
+    double preco;
+
+    public boolean temMesmoPreco(ComprarProduto outro){
+        return this.preco == outro.preco;
+    }
+    public static void main(String[] args){
+        ComprarProduto p1 = new ComprarProduto();
+        p1.nome = "Caneta";
+        p1.preco = 2.5;
+
+        ComprarProduto p2 = new ComprarProduto();
+        p2.nome = "Lápis";
+        p2.preco = 2.5;
+
+        System.out.println(p1.temMesmoPreco(p2));
+    }
+
 }
