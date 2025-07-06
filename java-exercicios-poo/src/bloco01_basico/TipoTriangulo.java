7b8646865f985510818df2661c4345f3f95bc005
hebertaragao <hebert.aragao@outlook.com>
2025-07-06 14:00:38 -0300

    Update TipoTriangulo.java class

--- java-exercicios-poo/src/bloco01_basico/TipoTriangulo.java
+++ java-exercicios-poo/src/bloco01_basico/TipoTriangulo.java
@@ -2,4 +2,18 @@ package bloco01_basico;
 
 public class TipoTriangulo {
 
+    public String tipoTriangulo(int a, int b, int c){
+        if (a == b && b == c) return "Equilátero";
+        else if ( a == b || b == c || a == c) return "Isósceles";
+        else return "Escaleno";
+    }
+
+    public static void main(String[] args){
+        TipoTriangulo t = new TipoTriangulo();
+        System.out.println(t.tipoTriangulo(3, 3, 3)); // Equilátero
+        System.out.println(t.tipoTriangulo(3, 4, 3)); // Isósceles
+        System.out.println(t.tipoTriangulo(3, 4, 5)); //Escalaneo
+
+    }
+
 }
