013bc266629bc16013f148ed361bf4b09381b272
hebertaragao <hebert.aragao@outlook.com>
2025-07-06 13:43:33 -0300

    Update ContadorUsuarios.java class

--- java-exercicios-poo/src/bloco01_basico/ContadorUsuarios.java
+++ java-exercicios-poo/src/bloco01_basico/ContadorUsuarios.java
@@ -2,4 +2,18 @@ package bloco01_basico;
 
 public class ContadorUsuarios {
 
+    static int totalUsuarios = 0;
+    String nome;
+
+    public ContadorUsuarios(String nome){
+        this.nome = nome;
+        totalUsuarios++;
+    }
+
+    public static void main(String[] args){
+        new ContadorUsuarios("Ana");
+        new ContadorUsuarios("Carlos");
+        System.out.println("Usuarios criados: " + ContadorUsuarios.totalUsuarios);
+    }
+
 }
