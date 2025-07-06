113ce2d0cbc20e2d456e55d475f1f38e05a4afaa
hebertaragao <hebert.aragao@outlook.com>
2025-07-06 13:53:46 -0300

    Update ConversorTemperatura.java class

--- java-exercicios-poo/src/bloco01_basico/ConversorTemperatura.java
+++ java-exercicios-poo/src/bloco01_basico/ConversorTemperatura.java
@@ -1,5 +1,19 @@
 package bloco01_basico;
 
 public class ConversorTemperatura {
+    public double celsiusParaFarenheit(double c){
+        return (c * 9/5) + 32;
+    }
+
+    public double farenheitParaCelisius(double f){
+        return (f - 32) * 5/9;
+    }
+
+    public static void main(String[] args){
+        ConversorTemperatura conv = new ConversorTemperatura();
+        System.out.println("25ºC = " + conv.celsiusParaFarenheit(25) + "ºF");
+        System.out.println("77ºF = " + conv.farenheitParaCelisius(77) + "ºC");
+
+    }
 
 }
